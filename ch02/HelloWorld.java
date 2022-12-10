3f42d8fe559a2cecfc4da99f0018610373abafba
Marc Loy <marc@loylabs.com>
2022-12-10 07:35:25 -0500

    Initial examples for ch02

---
+++ ch02/HelloWorld.java
@@ -0,0 +1,10 @@
+package ch02;
+
+/**
+ * The canonical first program: Hello, world!
+ */
+public class HelloWorld {
+  public static void main( String[] args ) {
+    System.out.println("Hello, world!");
+  }
+}
