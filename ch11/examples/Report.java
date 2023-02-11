187fbde853e8ec750bc7c161405d108fb9bbbd39
Marc Loy <marc@loyinc.com>
2023-02-11 08:52:05 -0500

    Adding a few ch11 examples

---
+++ ch11/examples/Report.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Report {
+  List<PaidEmployee> employees = new ArrayList<>();
+
+  void buildEmployeeList() {
+    employees.add(new PaidEmployee("Fozzi", 4, 30_000));
+    employees.add(new PaidEmployee("Gonzo", 2, 50_000));
+    employees.add(new PaidEmployee("Kermit", 1, 60_000));
+    employees.add(new PaidEmployee("Piggy", 3, 80_000));
+  }
+
+  public void publishNames() {
+    employees.stream().map(e -> e.getName()).forEach(System.out::println);
+  }
+
+  public static void main(String args[]) {
+    Report r = new Report();
+    r.buildEmployeeList();
+    r.publishNames();
+  }
+}
+
