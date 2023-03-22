71a0ef32290079b42eca952154a63fe742b11bee
Marc Loy <marc@loylabs.com>
2023-03-22 19:34:28 -0400

    Rename an error exercise and add a compile note

--- ch06/exercises/Pause.java
+++
@@ -1,9 +0,0 @@
-package ch06.exercises;
-
-public class Pause {
-  public static void main(String args[]) {
-    System.out.print("Starting... ");
-    Thread.sleep(5000);
-    System.out.println("done");
-  }
-}
